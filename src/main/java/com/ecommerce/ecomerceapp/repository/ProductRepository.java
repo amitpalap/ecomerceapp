6cd5d3cf147391196fcc09cb46802afd547e8753
sudhir kumar <sudhir.kumar@netcracker.com>
2023-08-19 14:30:29 +0530

    sudhir kumar commit

--- src/main/java/com/ecommerce/ecomerceapp/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.ecomerceapp.repository;
-
-import com.ecommerce.ecomerceapp.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
--- src/main/java/com/ecommerce/ecomerceapp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.ecomerceapp.repository;
-
-import com.ecommerce.ecomerceapp.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-}
